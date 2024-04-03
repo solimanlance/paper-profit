4833a9ef720787e51214cd6c91a07efb75b1d9cd
lancesoliman <lsolim01@student.ubc.ca>
2024-04-02 21:35:34 -0700

    added event test

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -21,18 +21,18 @@ public class EventTest {
 
     @BeforeEach
     public void runBefore() {
-        e = new Event("Sensor open at door");   // (1)
+        e = new Event("lance bought 2 share(s) of AMZN");   // (1)
         d = Calendar.getInstance().getTime();   // (2)
     }
 
-//    @Test
-//    public void testEvent() {
-//        assertEquals("Sensor open at door", e.getDescription());
-//        assertEquals(d, e.getDate());
-//    }
-//
-//    @Test
-//    public void testToString() {
-//        assertEquals(d.toString() + "\n" + "Sensor open at door", e.toString());
-//    }
+    @Test
+    public void testEvent() {
+        assertEquals("lance bought 2 share(s) of AMZN", e.getDescription());
+        assertEquals(d, e.getDate());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(d.toString() + "\n" + "lance bought 2 share(s) of AMZN", e.toString());
+    }
 }
